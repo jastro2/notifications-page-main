bc8eb0d5650cb83c55d5015597f2347caaf9c1e6
Jordan <47327993+jastro2@users.noreply.github.com>
2024-07-02 17:54:07 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import BootstrapVue3 from 'bootstrap-vue-3';
 
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
+
+// import BootstrapVue3 from 'bootstrap-vue-3';
 
 import PostBoi from './components/PostBoi.vue';
 
 const app = createApp(App);
 
-app.use(BootstrapVue3);
-app.use(IconsPlugin);
+// app.use(BootstrapVue3);
 
 app.component('post-boi', PostBoi)
 
