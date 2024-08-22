42a46e40c731f80410fc42c5f579d47e6bdf6c2a
Jordan <47327993+jastro2@users.noreply.github.com>
2024-08-22 13:34:29 -0400

    Update PostBoi.vue

--- src/components/PostBoi.vue
+++ src/components/PostBoi.vue
@@ -1,13 +1,31 @@
 <template>
-    <div class="container">
-        <div @click="toggleRead()" class="row">  
-            <div class="notification-container">
-                <img :src="id" alt="">
-                <h2>{{name}}</h2>
-                <p class="action">{{ action }}</p>
-                <p class="action-info">{{ actionInfo }}</p>
-                <img class="photo" :src="photo">
-            </div>              
+    <div class="container mx-0 px-0">
+        <div @click="toggleRead()" class="notification-container rounded-2" :class="{ notificationIsUnread: notificationIsUnread}">
+            <div class="row align-items-start">
+                <div class="col-1 profile-picture">
+                    <div class="d-flex">
+                        <img class="photo" :src="pfp">
+                    </div>
+                </div>
+                <div class="col-10">
+                    <div class="notification-text">
+                        <p><span class="name">{{name}}</span><span class="action mb-0">{{ action }}</span><span v-if="postInfo" class="post-info">{{ postInfo }}</span><span v-if="groupInfo" class="group-info">{{ groupInfo }}</span></p>
+                    </div>
+                    <div class="timestamp pt-1">
+                        <span>{{ timestamp }}</span>
+                    </div>
+                </div>
+                <div class="col-1 p-0 notification-photo">
+                    <div class="notification-img">
+                        <img v-if="photo" class="photo" :src="photo">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-11 offset-1 pe-md-0 pe-4">
+                        <p v-if="message" class="message-container mb-0 mt-2 rounded-2">{{ message }}</p>
+                    </div>
+                </div>
+            </div>        
         </div>
     </div>
 </template>
@@ -15,21 +33,23 @@
 <script>
     export default {
         props: [
-            'id',
+            'pfp',
             'name',
             'action',
-            'actionInfo',
+            'postInfo',
+            'groupInfo',
             'message',
-            'photo'
+            'photo',
+            'timestamp'
         ],
         data() {
             return {
-                notificationIsRead: false,
+                notificationIsUnread: true,
             }
         },
         methods: {
             toggleRead() {
-                this.notificationIsRead = !this.notificationIsRead;
+                this.notificationIsUnread = !this.notificationIsUnread;
             }
         }
     };
