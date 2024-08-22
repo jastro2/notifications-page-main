97580aa0b5bf8cfb473907928344ac7f03d4bb59
Jordan <47327993+jastro2@users.noreply.github.com>
2024-08-22 13:35:06 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 
-// import 'bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
-
-// import BootstrapVue3 from 'bootstrap-vue-3';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import PostBoi from './components/PostBoi.vue';
 
